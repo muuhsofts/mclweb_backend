8b79bce60980ed1bc37277cae99ebbed07d05f99
muuhsofts <mudhihirnyemasoft01@gmail.com>
2026-04-09 10:44:38 +0300

    solve gallery page

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -16,7 +16,7 @@ class GalleryController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:sanctum')->except(['index', 'show']);
+        $this->middleware('auth:sanctum')->except(['index', 'show','fetchAllGallery']);
     }
 
     public function index()
